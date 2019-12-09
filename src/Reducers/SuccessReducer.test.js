24bdbfe31f48943f814528d822fdfb9d1d07ae73
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-12-09 15:07:30 -0500

    Write code and test for successReducer

---
+++ src/Reducers/SuccessReducer.test.js
@@ -0,0 +1,13 @@
+import { actionTypes } from '../Actions/Actions'
+import successReducer from './SuccessReducer'
+
+test('returns default state when no action is passed', () => {
+  const newState = successReducer(undefined, {})
+  expect(newState).toBe(false)
+
+})
+test('returns state of true upon receiving an action of type CORRECT_GUESS', () => {
+
+  const newState = successReducer(undefined, { type: actionTypes.CORRECT_GUESS })
+  expect(newState).toBe(true)
+})
